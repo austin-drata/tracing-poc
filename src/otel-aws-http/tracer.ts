cdad6ba9a836506aeffb2c03144b36a26b908b39
Emily Parr <emily@drata.com>
2024-02-26 16:49:36 -0800

    updating basic trace example

--- src/otel-aws-http/tracer.ts
+++ src/otel-aws-http/tracer.ts
@@ -1,56 +1,35 @@
-import opentelemetry from "@opentelemetry/api";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import opentelemetry from '@opentelemetry/api';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { AwsInstrumentation } from '@opentelemetry/instrumentation-aws-sdk';
-import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
-import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-import { Resource } from "@opentelemetry/resources";
-import { NodeSDK } from "@opentelemetry/sdk-node";
-import { ConsoleSpanExporter, SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { Resource } from '@opentelemetry/resources';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 
-export const getTracer = () => {
-  const provider = new NodeTracerProvider({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: process.env.DD_SERVICE,
-    }),
-  });
-
-  provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-
-  // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
-  provider.register();
-
-  registerInstrumentations({
-    instrumentations: [
-        new ExpressInstrumentation(),
-        new HttpInstrumentation(),
-    ],
-  });
-
-  return opentelemetry.trace.getTracer('http-example');
-};
-
 export const getTracerSdk = () => {
-    const resource = new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: process.env.DD_SERVICE,
-    });
+  const resource = new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: process.env.DD_SERVICE,
+  });
 
-    const exporter = new ConsoleSpanExporter();
+  // const exporter = new ConsoleSpanExporter();
+  const exporter = new OTLPTraceExporter({
+    url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT 
+  });
 
-    const sdk = new NodeSDK({
-      traceExporter: exporter,
-      resource,
-      instrumentations: [
-        new AwsInstrumentation({ suppressInternalInstrumentation: true }),
-        new ExpressInstrumentation(),
-        new HttpInstrumentation(),
-      ],
-      spanProcessor: new SimpleSpanProcessor(exporter),
-    });
+  const sdk = new NodeSDK({
+    traceExporter: exporter,
+    resource,
+    instrumentations: [
+      new AwsInstrumentation({ suppressInternalInstrumentation: true }),
+      new HttpInstrumentation(),
+    ],
+    spanProcessor: new SimpleSpanProcessor(exporter),
+  });
 
-    return {
-        sdk,
-        tracer: opentelemetry.trace.getTracer('http-example-sdk')
-    }
-}
+  return {
+    sdk,
+    tracer: opentelemetry.trace.getTracer('http-example-sdk'),
+  };
+};
